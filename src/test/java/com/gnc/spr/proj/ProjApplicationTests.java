3ba6dd3b2108675b716dcbc54c512af96ecb96a4
divyang7777 <divyangmistry7777@gmail.com>
2020-02-08 15:45:31 +0530

    initial commit

---
+++ src/test/java/com/gnc/spr/proj/ProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnc.spr.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
